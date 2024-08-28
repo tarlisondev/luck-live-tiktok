0a651b3623e2ed76a6703061e90a47ac1ec11e89
unknown <tarlison.dev@gmail.com>
2024-08-28 12:43:13 -0300

    luck-live-tiktok-client

--- src/controllers/raffle.js
+++ src/controllers/raffle.js
@@ -3,7 +3,12 @@ const Raffle = require("../models/raffle");
 
 const findRaffle = async (_, res) => {
     try {
-        res.render("pages/raffle", { data: await Raffle.find(), title: "Rifas" });
+        const raffle = await Raffle.findOne();
+        let percentual = ((raffle.numbers.length / raffle.amount) * 100).toFixed(2);
+
+        console.log(percentual+"%");
+
+        res.render("pages/raffle", { data: raffle, percentual: percentual, title: "Rifas",  });
     } catch (error) { console.log(error) }
 }
 
